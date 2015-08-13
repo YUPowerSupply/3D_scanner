b6746e13e23c5769c83b03482e468b2965ead25c
kim <snk1587@gmail.com>
2015-08-13 20:37:47 +0900

    pcl add first example

---
+++ build-ex_pcl-Desktop-Default/CMakeFiles/ex_pcl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ex_pcl.dir/main.cpp.o"
+  "ex_pcl.pdb"
+  "ex_pcl"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ex_pcl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build-start_openni-Desktop-Default/CMakeFiles/start_openni.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/start_openni.dir/main.cpp.o"
+  "start_openni.pdb"
+  "start_openni"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/start_openni.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
